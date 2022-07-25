80900d0525280b5194bbc5135c972fdc6732b933
Enrique De Jesús Espinoza Salas <terryrockstar22@gmail.com>
2022-07-25 18:00:01 -0500

    First commit

---
+++ src/Challenge0.kt
@@ -0,0 +1,16 @@
+
+/*
+* Challenge #0
+* ANAGRAM
+* Date: 01/08/2022
+* Difficulty: Medium
+*
+* Write a function that receives two words (String) and returns true or false (Boolean) depending on whether or not they are anagrams.
+* - An Anagram consists of forming a word by rearranging ALL the letters of another initial word.
+* - It is NOT necessary to check that both words exist.
+* - Two exactly the same words are not an anagram.
+*/
+
+fun main(){
+    print("l")
+}
