5af9f3946a19ac6c6f7d1d968144410fde5d2ca3
Manuel Lopez <juan.lopez@occ.com.mx>
2022-08-01 10:49:46 -0500

    Update Challenge0.kt

--- src/Challenge0.kt
+++ src/Challenge0.kt
@@ -7,6 +7,19 @@
 *
 */
 
-fun main(){
-    print("Hello world")
+fun main() {
+    println(esAnagrama("Valora", "alvaro"))
+}
+
+fun esAnagrama(palabra1: String, palabra2: String): Boolean {
+    if (palabra1.lowercase() == palabra2.lowercase() || palabra1.length != palabra2.length) return false
+    var anagrama = true
+    for (letra in palabra1) {
+        anagrama = palabra2.contains(letra.lowercase())
+        if(!anagrama || palabra1.lowercase().split(letra).size != palabra2.lowercase().split(letra).size) return false
+    }
+    for (letra in palabra2) {
+        anagrama = palabra1.lowercase().contains(letra.lowercase())
+    }
+    return anagrama
 }
