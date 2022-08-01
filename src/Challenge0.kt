38f812b6a1b9eb690f89424dc4c65ecc940e7315
Enrique Espinoza Salas <eespinoza@occ.com.mx>
2022-08-01 10:24:26 -0500

    docs: Add Challenge0 information

--- src/Challenge0.kt
+++ src/Challenge0.kt
@@ -1,10 +1,20 @@
 
 /*
 * Challenge #0
-*
+* ANAGRAM
 * Date: 01/08/2022
-* Difficulty:
+* Difficulty: Medium
 *
+* Roma Amor = true
+* roma amor = true
+* vacio enfermo = false
+* amor amor = false
+*
+*
+* Write a function that receives two words (String) and returns true or false (Boolean) depending on whether or not they are anagrams.
+* - An Anagram consists of forming a word by rearranging ALL the letters of another initial word.
+* - It is NOT necessary to check that both words exist.
+* - Two exactly the same words are not an anagram.
 */
 
 fun main(){
