fcda73902d35aee3363b736fb6de1665f2d09d54
Enrique Vázquez Rodríguez <45948551+enriquevazrod@users.noreply.github.com>
2022-08-01 10:49:20 -0500

    Mi respuesta

--- src/Challenge0.kt
+++ src/Challenge0.kt
@@ -8,5 +8,31 @@
 */
 
 fun main(){
-    print("Hello world")
+    print("Las palabras son: " + anagram("anita", "tania"))
+
+}
+
+fun anagram(wordOne: String, wordTwo: String): Boolean{
+
+    val validation: Boolean = if (wordOne == wordTwo){
+        false
+    }else {
+        if (wordOne.length == wordTwo.length){
+            val first: CharArray = toCharacterArray(wordOne)
+            println("First Word: $first")
+            val second: CharArray = toCharacterArray(wordTwo)
+            println("Second Word: $second")
+            println("Result: ${first.toTypedArray() contentEquals second.toTypedArray()}")
+            first.toTypedArray() contentEquals second.toTypedArray()
+        }else{
+            false
+        }
+
+    }
+
+    return validation
+}
+
+fun toCharacterArray(str: String): CharArray {
+    return str.toCharArray()
 }
