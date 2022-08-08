c18a46fb39a00b6de90a2552bd8407f4fd48af58
Enrique De Jesús Espinoza Salas <terryrockstar22@gmail.com>
2022-08-08 09:17:50 -0500

    chore: Update Challenge1

--- src/Challenge1.kt
+++ src/Challenge1.kt
@@ -3,9 +3,13 @@
 *
 * Date: 08/08/2022
 * Difficulty: Medium
+* Write a function that receives an array of int and returns the second-highest value
+* - Check Execution time complexity
+* - Check Cognitive complexity
 */
 
 fun main(){
+    // [1, 6, 8, 4, 9, 12, 30, 15, 2, 4, 7, 3, 10, 4, 7, 11, 14]
     measureTimeMillis { print("Hello world!") }
 }
 
