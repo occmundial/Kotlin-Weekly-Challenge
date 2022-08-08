02b5444000df1f61397999a52c220c22eef56bca
Enrique Vázquez Rodríguez <45948551+enriquevazrod@users.noreply.github.com>
2022-08-08 10:47:31 -0500

    Mi respuesta a ChallengeOneEVR

--- src/Challenge1.kt
+++ src/Challenge1.kt
@@ -10,7 +10,6 @@
 
 fun main(){
     val array = arrayOf(1, 6, 8, 4, 9, 12, 30, 15, 2, 4, 7, 3, 10, 4, 7, 11, 14)
-    //measureTimeMillis { print("Hello world!") }
     measureTimeMillis { println("the second highest value is: ${returnsTSHV(array)}") }
 }
 
@@ -25,13 +24,7 @@ fun returnsTSHV(arrayList: Array<Int>): Int {
     return arrayList.sortedDescending()[1]
 }*/
 
-/* 24 millis
-fun returnsTSHV(arrayList: Array<Int>): Int {
-    val secondNumber = arrayList.sorted().size
-    return arrayList.sorted()[secondNumber - 2]
-}*/
-
-//
+// 24 millis
 fun returnsTSHV(arrayList: Array<Int>): Int {
     val secondNumber = arrayList.sorted().size
     return arrayList.sorted()[secondNumber - 2]
