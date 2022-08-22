e5c27dfd630ed66cc35c7ac1192599d50a7427d6
Enrique De Jesús Espinoza Salas <terryrockstar22@gmail.com>
2022-08-22 08:56:38 -0500

    Add Challenge 4

--- src/Challenge3.kt
+++ src/Challenge3.kt
@@ -3,6 +3,10 @@
 *
 * Date: 22/08/2022
 * Difficulty: Medium
+* Create ONE SINGLE FUNCTION (important that it is only one) that is capable of calculating and returning the area of a polygon.
+* - The function will receive by parameter only ONE polygon at a time.
+* - The supported polygons will be Triangle, Square and Rectangle.
+* - Prints the calculation of the area of a polygon of each type.
 *
 */
 fun main() {
