f7a0e418f92290b70beae102848e4711aef2333a
Manuel Lopez <juan.lopez@occ.com.mx>
2022-08-22 10:48:55 -0500

    Update Challenge3.kt

--- src/Challenge3.kt
+++ src/Challenge3.kt
@@ -9,6 +9,27 @@
 * - Prints the calculation of the area of a polygon of each type.
 *
 */
+
+data class Poligono(
+    val base: Float,
+    val altura: Float,
+    val sides: ArrayList<Float>
+)
 fun main() {
-    println("Hello Kotlin")
+    val triangulo = Poligono(8.0f, 6.92f, arrayListOf(8.0f, 8.0f, 8.0f))
+    val cuadrado = Poligono(10.0f, 10.0f, arrayListOf(10.0f, 10f, 10f, 10.0f))
+    val rectangulo = Poligono(10.0f, 8.0f, arrayListOf(8.0f, 10f, 8f, 10.0f))
+    println(calculateArea(triangulo))
+}
+
+fun calculateArea(poligono: Poligono): String {
+    return when(poligono.sides.size) {
+        3 -> "Triangulo: ${(poligono.base * poligono.altura) / 2}"
+        4 -> if (poligono.base == poligono.altura) {
+                "Cuadrado: ${poligono.sides[0] * poligono.sides[1]}"
+            } else {
+                "Rectangulo: ${poligono.base * poligono.altura}"
+            }
+        else -> "Error"
+    }
 }
