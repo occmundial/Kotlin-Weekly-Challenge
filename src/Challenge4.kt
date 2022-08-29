11eb0167284fd4eff2ff6784e2c5fa914ed84b8d
Enrique Vázquez Rodríguez <enrique.vazquez@occ.com.mx>
2022-08-29 10:41:36 -0500

    Challenge

--- src/Challenge4.kt
+++ src/Challenge4.kt
@@ -8,5 +8,35 @@
 *
 */
 fun main() {
-    println("Hello Kotlin")
+    //contFor()
+    //contWhile()
+    contDoWhile()
+}
+
+private fun contFor(){
+    for (elemento in 1..100){
+        printElement(elemento)
+    }
+}
+
+private fun contWhile(){
+    var count = 0
+
+    while (count <= 100){
+        printElement(count)
+        count++
+    }
+}
+
+private fun contDoWhile(){
+    var count = 0
+
+    do{
+        printElement(count)
+        count++
+    }while (count <= 100)
+}
+
+private fun printElement(number: Int){
+    println("El numero actual es: $number")
 }
