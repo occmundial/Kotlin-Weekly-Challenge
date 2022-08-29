b0f690c7446984c3907a0a1320fd3454e31310b8
Enrique De Jesús Espinoza Salas <terryrockstar22@gmail.com>
2022-08-29 09:50:52 -0500

    chore: Add Challenge 4

--- src/Challenge4.kt
+++ src/Challenge4.kt
@@ -4,4 +4,9 @@
 * Date: 29/08/2022
 * Difficulty: Easy
 *
+* I want to count from 1 to 100 one by one (printing each one). How are you able to do it? Create the code for each of them.
+*
 */
+fun main() {
+    println("Hello Kotlin")
+}
