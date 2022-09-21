dc3c099ba62f40b88dc3de8e86e87e22566f8386
Enrique Espinoza Salas <eespinoza@occ.com.mx>
2022-09-21 11:51:13 -0500

    feat: Add Challenge5

--- src/Challenge5.kt
+++ src/Challenge5.kt
@@ -1,12 +1,28 @@
 /*
 * Challenge #5
 *
-* Date: 05/09/2022
+* Date: 21/09/2022
 * Difficulty: Easy
 *
-* Write a function that displays on the console (with a print) the numbers from 1 to 1000 (both included and with a line break between each print), substituting the following:
+* Write a function that displays on the console (with a print) the numbers from 1 to 1000 (both included and with a line break between each print),
+* substituting the following:
 * - Multiples of 3 for the word "fizz".
 * - Multiples of 5 for the word "buzz".
 * - Multiples of 3 and 5 at the same time for the word "fizzbuzz".
 *
 */
+fun main() {
+    (1..1_000).forEach { index ->
+        val divisibleByThree = index % 3 == 0
+        val divisibleByFive = index % 5 == 0
+        var num = index.toString()
+        if (divisibleByThree && divisibleByFive) {
+            num = "fizzbuzz"
+        } else if (divisibleByThree) {
+            num = "fizz"
+        } else if (divisibleByFive){
+            num = "buzz"
+        }
+        println(num)
+    }
+}
