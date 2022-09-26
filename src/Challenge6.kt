53c02e5853fd37a592ffcc8c9dc03c5ebbe4acb9
Enrique Espinoza Salas <eespinoza@occ.com.mx>
2022-09-26 11:29:12 -0500

    Add Solution Challenge6

--- src/Challenge6.kt
+++ src/Challenge6.kt
@@ -7,3 +7,11 @@
 * Write a function that calculates and returns the factorial of a given number recursively.
 *
 */
+fun main() {
+    println(factorial(0) ?: run { "No tiene factorial" })
+    println(factorial(-1) ?: run { "No tiene factorial" })
+}
+
+private fun factorial(n: Int): Int? {
+    return if (n < 0) null else if (n <= 1) 1 else n * (factorial(n - 1)!!)
+}
