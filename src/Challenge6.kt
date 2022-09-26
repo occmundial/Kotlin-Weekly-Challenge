40d4d35ec24b60c8f46f75abcdf1f9adfe784845
Enrique Vázquez Rodríguez <enrique.vazquez@occ.com.mx>
2022-09-26 10:35:26 -0500

    Challenge 6 Factorial

--- src/Challenge6.kt
+++ src/Challenge6.kt
@@ -7,3 +7,26 @@
 * Write a function that calculates and returns the factorial of a given number recursively.
 *
 */
+
+
+fun main(){
+
+    val number = 5.0
+
+    if (number.isFinite()){
+        println("Este numero decimal no puede tener factorial")
+    }else if (number <= 0){
+        println("Este numero no puede tener factorial")
+    }else{
+        println("El factorial de $number es: ${Factorial(number.toInt())}")
+    }
+
+}
+fun Factorial(number: Int) : Int {
+
+    if (number <= 1){
+        return 1
+    }
+
+    return number * Factorial(number - 1)
+}
