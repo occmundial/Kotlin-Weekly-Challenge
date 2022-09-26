ca776daa3ac7d9d3682564c1afcb82f28264f14f
Manuel Lopez <juan.lopez@occ.com.mx>
2022-09-26 10:33:17 -0500

    Update Challenge6.kt

--- src/Challenge6.kt
+++ src/Challenge6.kt
@@ -7,3 +7,15 @@
 * Write a function that calculates and returns the factorial of a given number recursively.
 *
 */
+
+fun main() {
+    println(factorial(6U))
+}
+
+fun factorial(number: UInt): UInt {
+    return if (number > 0U) {
+        number * factorial(number -1U)
+    } else {
+        1U
+    }
+}
