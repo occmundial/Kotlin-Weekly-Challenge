02bc2386eeb327480a75b800e3a46ea86581a545
Enrique Vázquez Rodríguez <45948551+enriquevazrod@users.noreply.github.com>
2022-10-05 16:35:56 -0500

    Polimorfismo EVR 2

--- src/Challenge7.kt
+++ src/Challenge7.kt
@@ -14,3 +14,47 @@
 * - Attack: Between 1 and 100.
 * - Defense: Between 1 and 100.
 */
+
+fun main(){
+    println(startBattle(WATER, 10, 100, FIRE, 100, 50))
+}
+
+fun startBattle(typePokemon1: Int, attack1: Int, defense1: Int, typePokemon2: Int, attack2: Int, defense2: Int): String{
+
+    var winner = ""
+
+    if (getDamage(attack1, defense1, getEffectiveness(typePokemon1)) < getDamage(attack2, defense2, getEffectiveness(typePokemon2))){
+        winner = "fue un empate"
+    }else if (getDamage(attack1, defense1, getEffectiveness(typePokemon1)) < getDamage(attack2, defense2, getEffectiveness(typePokemon2))){
+        winner = "Gano el pokemon 2"
+    }else {
+        winner = "Gano el pokemon 1"
+    }
+
+    return winner
+}
+
+fun getDamage(attack: Int, defense: Int, effectiveness: Int) : Int {
+    return 50 * (attack / defense) * effectiveness
+}
+
+fun getEffectiveness(typePokemon: Int): Int{
+
+    var effectiveness = 0
+
+    when(typePokemon){
+        WATER ->{ }
+    }
+
+    if (typePokemon == WATER ){
+
+
+    }
+
+    return 1
+}
+
+const val WATER = 1    // 2 -3
+const val FIRE = 2     // 3 -1
+const val GRASS = 3    // 1 -2
+const val ELECTRIC = 4 // 1 -3
