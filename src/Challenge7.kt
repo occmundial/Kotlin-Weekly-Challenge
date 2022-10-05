75927228b0badc516f590a830016d1fa44cf9512
Manuel Lopez <juan.lopez@occ.com.mx>
2022-10-05 16:26:29 -0500

    Update Challenge7.kt

--- src/Challenge7.kt
+++ src/Challenge7.kt
@@ -14,3 +14,32 @@
 * - Attack: Between 1 and 100.
 * - Defense: Between 1 and 100.
 */
+
+lateinit var effectiveness: Map<String, Float>
+val types = arrayOf("Water", "Fire", "Electric", "Grass")
+fun main() {
+    println(superBattle("Fire", "Fire", 80, 20))
+}
+
+fun superBattle(typeAttack: String, typeDefending: String, attack: Int, defense: Int): Float? {
+    if (!types.contains(typeAttack) || !types.contains(typeDefending)) {
+        print("No existe el tipo")
+        return null
+    }
+    if (attack !in 1..100 || defense !in 1..100) {
+        print("Ingresa un valor entre 1 y 100")
+        return null
+    }
+    effectiveness = mapOf(
+        "WaterFire" to 2f,
+        "WaterElectric" to 0.5f,
+        "FireGrass" to 2f,
+        "FireWater" to 0.5f,
+        "GrassWater" to 2f,
+        "GrassFire" to 0.5f,
+        "ElectricWater" to 2f,
+        "ElectricGrass" to 0.5f,
+    )
+    val effect: Float = effectiveness[typeAttack + typeDefending] ?: 1f
+    return 50 * (attack / defense) * effect
+}
