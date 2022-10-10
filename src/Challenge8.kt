003d1036daccc8eff65321d2cd97d0510dd3e785
Enrique Vázquez Rodríguez <45948551+enriquevazrod@users.noreply.github.com>
2022-10-10 11:17:39 -0500

    Challenge 8

--- src/Challenge8.kt
+++ src/Challenge8.kt
@@ -1,3 +1,5 @@
+import java.util.*
+
 /*
 * Challenge #8
 *
@@ -16,3 +18,83 @@
  * - You have complete freedom to model the exercise data.
  * Ex: 1 Hairy loses against 1 Orc, 2 Hairy tie against 1 Orc, 3 Hairy win against 1 Orc.
  */
+
+
+lateinit var naturalDictionaryKind: Map<String, Int>
+lateinit var naturalDictionaryEvil: Map<String, Int>
+fun main(){
+
+    createDictionariesRangesKind()
+    createDictionariesRangesEvil()
+
+    //Primer Ejercito
+    val mapKind = TreeMap<String, Int>()
+    mapKind["Furry"] = 10
+    mapKind["Good Southerners"] = 2
+    mapKind["Dwarves"] = 3
+    mapKind["Númenóreans"] = 40
+    mapKind["Elves"] = 5
+
+    //vs Segundo ejercito
+    val mapEvil = TreeMap<String, Int>()
+    mapEvil["Evil Southlanders"] = 10
+    mapEvil["Orcs"] = 2
+    mapEvil["Goblins"] = 3
+    mapEvil["Wargs"] = 40
+    mapEvil["Trolls"] = 5
+
+    //Resultado de la batalla
+}
+
+private fun batalla(ejercito1: TreeMap<String, Int>, ejercito2: TreeMap<String, Int>): String{
+
+    
+
+
+    return ""
+}
+
+private fun powerBattle(ejercito1: TreeMap<String, Int>): Int{
+    var totalpower: Int = 0
+
+    for((race, numberOfSoldiers) in ejercito1){
+
+
+
+        println("$race : $numberOfSoldiers")
+
+        val powerrange = naturalDictionaryKind[race]
+
+        totalpower += (powerrange!! * numberOfSoldiers)
+    }
+
+    return 1
+}
+
+private fun createDictionariesRangesKind() {
+    naturalDictionaryKind = mapOf(
+        "Furry" to 1,
+        "Good Southerners" to 2,
+        "Dwarves" to 3,
+        "Númenóreans" to 4,
+        "Elves" to 5
+    )
+
+    naturalDictionary.forEach {
+        morseDictionary[it.value] = it.key
+    }
+}
+
+private fun createDictionariesRangesEvil() {
+    naturalDictionaryEvil = mapOf(
+        "Evil Southlanders" to 2,
+        "Orcs" to 2,
+        "Goblins" to 2,
+        "Wargs" to 3,
+        "Trolls" to 5
+    )
+
+    naturalDictionary.forEach {
+        morseDictionary[it.value] = it.key
+    }
+}
