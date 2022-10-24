1a825eae589ac2dcc887106c3f7c1d983eb2388e
Enrique De Jesús Espinoza Salas <terryrockstar22@gmail.com>
2022-10-24 09:57:10 -0500

    Add Challenge 10 information

---
+++ src/Challenge10.kt
@@ -0,0 +1,16 @@
+/*
+* Challenge #10
+* THREE IN A ROW
+* Date: 24/10/2022
+* Difficulty: Medium
+*
+* Create a function that parses a 3x3 matrix made up of "X" and "O" and returns the following:
+* - "X" if they have won the "X"
+* - "O" if they have won the "O"
+* - "Draw" if there was a draw
+* - "Null" if the proportion of "X", of "O", or of the matrix is not correct. Or if both have won.
+* Note: Matrix may not be fully covered. It could be represented with an empty "", for example.
+*/
+fun main() {
+    println("THREE IN A ROW")
+}
