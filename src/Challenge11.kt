5b62ac9a42cf7210d9cbdbffd2f9978b7d938768
Enrique De Jesús Espinoza Salas <terryrockstar22@gmail.com>
2022-10-31 09:44:10 -0600

    Add Challenge 11 information

---
+++ src/Challenge11.kt
@@ -0,0 +1,33 @@
+
+/*
+* Challenge #10
+* THREE IN A ROW
+* Date: 31/10/2022
+* Difficulty: Medium
+*
+* We must create a program to which we indicate if we want to perform "Trick or Treat"
+* and a list (array) of people with the following properties:
+*
+* - Name of the girl or boy
+* - Age
+* - Height in centimeters
+*
+* If people have asked for a cheat, the program will return scares (randomly)
+* following these criteria:
+*
+* - One scare for every 2 letters of the name per person
+* - Two scares for each age that is an even number
+* - Three frights for every 100 cm of height among all people
+* - Scares: 🎃 👻 💀 🕷 🕸 🦇
+*
+* If people have asked for treatment, the program will return sweets (random)
+* following these criteria:
+*
+* - One candy for each letter of name
+* - One sweet for every 3 years up to a maximum of 10 years per person
+* - Two sweets for every 50 cm of height up to a maximum of 150 cm per person
+* - Sweets: 🍰 🍬 🍡 🍭 🍪 🍫 🧁 🍩
+*/
+fun main() {
+    println("Hello Halloween 🎃 👻 💀 🕷 🕸 🦇")
+}
